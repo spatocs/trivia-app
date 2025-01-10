9d19bbf9ae0f0d0ff11577b1bca7befb4be9ad22
Sandor - AWS Trainee <sandor+awsbuilder@patocspack.com>
2025-01-10 15:11:59 -0700

    Fix executable paths in local_build.sh

--- local_build.sh
+++ local_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pylint back-end-python/gameactions/app.py
-pytest back-end-python/tests/unit --cov-report=html --cov=gameactions --cov-branch
+bin/pylint back-end-python/gameactions/app.py
+bin/pytest back-end-python/tests/unit --cov-report=html --cov=gameactions --cov-branch
